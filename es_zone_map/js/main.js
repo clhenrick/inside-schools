d7d4e132f256a4a5849b584c6f04862ff8f1bfbb
Chris Henrick <chrishenrick@gmail.com>
2014-02-13 16:30:42 -0500

    added disclaimer label and fixed zone highlighting style bug

--- es_zone_map/js/main.js
+++ es_zone_map/js/main.js
@@ -1,5 +1,9 @@
 app = {
 	map : null,
+	esZones : null,
+	data : null,
+	target: null,
+
 	renderMap : function(){
 
 		var config = {
@@ -25,107 +29,126 @@ app = {
 	},
 
 	fetchData: function(){
-		var features,
-			zoneStyle,
-			esZones,
-			features,
-			len,
-			i,
-			labels,
-			coordinates,
-			target,
-			hStyle,
-			dStyle,
-			styleData;
-
-
 		$.getJSON('./ES_Zones_2013-2014.geojson', function(data){
-			console.log('./ES_Zones_2013-2014.geojson: ', data);
-
-			esZones = L.geoJson().addTo(app.map);
-			//esZones = L.geoJson(data, { style: app.dStyle }).addTo(app.map);
-
-			features = data.features;
-			len = features.length;
-			i=0;
-
-			// loop through school zone geojson features
-			for (i; i<len; i++){
-				labels = features[i].properties.Label;
-				coordinates = features[i].geometry.coordinates[0];
-				// variable for db query
-				target = "305";
-
-				// query features by zone id
-				if (labels === target) {
-					// console.log("i: ", i);
-					// console.log("PS305 coordinates[0]: ", coordinates);
-
-					styleData = function(feature){
-						switch(labels) {
-							case target: return app.hStyle;
-							case !target: return app.dStyle;
-							default: return app.dStyle;
-						}
-					},
-
-					esZones.addData(data);
-					esZones.setStyle(styleData());
-
-					// grab bounding box coordinates
-					var right = Math.max.apply(Math, coordinates.map(function(k) {
-							return k[0];
-						})),
-
-						left = Math.min.apply(Math, coordinates.map(function(k){
-							return k[0];
-						})),
-
-						top = Math.max.apply(Math, coordinates.map(function(k){
-							return k[1];
-						})),
-
-						bottom = Math.min.apply(Math, coordinates.map(function(k){
-							return k[1];
-						}));
-
-					// set arrarys for lower left and upper right 
-					var southWest = [bottom, left],
-						northEast = [top, right];
-
-					// debug
-					console.log('left: ', left, ' top: ', top, ' right: ', right, ' bottom: ', bottom);
-					console.log('southWest: ', southWest, ' northEast: ', northEast);
-
-					// zoom to bounding box
-					app.zoomToLayer(southWest, northEast);
-				}
-			}
+			//console.log('./ES_Zones_2013-2014.geojson: ', data);
+			app.data = data;
+			//console.log('app.data: ', data);
+			app.parseData(data);
 		});
 	},
+
+	parseData: function(data){
+		var geojson = data;
+		console.log('parseData data: ', data);
+		var features = geojson.features;
+		var len = features.length;
+		var i=0;
+		var labels, coordinates;
+
+		// loop through school zone geojson features
+		for (i; i<len; i++){
+			labels = features[i].properties.Label;
+			coordinates = features[i].geometry.coordinates[0];
+			// variable for db query
+			app.target = "305";
+
+			// query features by zone id
+			if (labels === app.target) {
+				// console.log("i: ", i);
+				// console.log("PS305 coordinates[0]: ", coordinates);
+
+				// add and style esZones
+				app.esZones = L.geoJson(app.data, {style: app.dStyle}).addTo(app.map);
+				app.initGeoJson(app.target);
+				// app.esZones.setStyle(styleData());
+
+				// grab bounding box coordinates
+				var right = Math.max.apply(Math, coordinates.map(function(k) {
+						return k[0];
+					})),
+
+					left = Math.min.apply(Math, coordinates.map(function(k){
+						return k[0];
+					})),
+
+					top = Math.max.apply(Math, coordinates.map(function(k){
+						return k[1];
+					})),
+
+					bottom = Math.min.apply(Math, coordinates.map(function(k){
+						return k[1];
+					}));
+
+				// set arrarys for lower left and upper right 
+				var southWest = [bottom, left],
+					northEast = [top, right];
+
+				// debug
+				console.log('left: ', left, ' top: ', top, ' right: ', right, ' bottom: ', bottom);
+				console.log('southWest: ', southWest, ' northEast: ', northEast);
+
+				// zoom to bounding box
+				app.zoomToLayer(southWest, northEast);
+			}
+		}
+	},
+
+	initGeoJson : function(n) {
+        console.log(app.esZones.options);
+        app.map.removeLayer(app.esZones);
+        if (n != "") {
+            sn = n;
+            console.log(sn);
+            geojson = L.geoJson(app.data, {
+                style: app.style
+            }).addTo(app.map);
+        }
+    },
+
+    style : function(feature) {
+        if (app.target === feature.properties.Label) {
+            return {
+                weight: 1,
+                opacity: 1,
+                color: 'white',
+                dashArray: '3, 5',
+                fillOpacity: 0.3,
+                fillColor: '#ff0000'
+            };
+            
+        } else {
+            return {
+                weight: 1,
+                opacity: 1,
+                color: 'white',
+                dashArray: '3, 5',
+                fillOpacity: 0.3,
+                fillColor: '#666666'
+            };
+        }
+    },
 
 	zoomToLayer : function(sw, ne) {
 		app.map.fitBounds([sw, ne], { padding: [10,10] });
 	},
 
 	hStyle : {
-	    "color": "#0000ff",
-	    "weight": 1,
-	    "opacity": 0.65
+	    color: "#0000ff",
+	    weight: 1,
+	    opacity: 1,
+	    dashArray: "1",
+	    fillOpacity: 0.3,
+	    fillColor: "#0000ff"
 	},
 
 	dStyle: {
-	    "color": "#ff0000",
-	    "weight": 1,
-	    "opacity": 0.65
+		weight: 1,
+	    color: "white",
+	    opacity: 1,
+	    dashArray: "1",
+	    fillOpacity: 0.3,
+	    fillColor: '#666666'
 	},
-
-	// styleData : function(feature){
-	// 	switch(labels) {
-	// 		case target: return app.hStyle;
-	// 		default: app.dStyle;
-	// 	}
-	// },
 
 	init : function(){
 		app.renderMap();
